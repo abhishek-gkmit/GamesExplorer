a6344d69e333b9c9e128356dd72561edf2f9faaf
Abhishek <abhishek@gkmit.co>
2024-12-20 11:35:36 +0530

    feat: add react query setup - install react query - create query client - add query client provider in App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { Provider } from 'react-redux';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import MainNavigator from '@config/MainNavigator';
 import store from '@store';
+import queryClient from '@network/queryClient';
 
 function App() {
   return (
     <Provider store={store}>
-      <MainNavigator />
+      <QueryClientProvider client={queryClient}>
+        <MainNavigator />
+      </QueryClientProvider>
     </Provider>
   );
 }
