d7f3c47ecb84a542a649b1ab88aac5628d876ed3
Abhishek <abhishek@gkmit.co>
2024-12-24 12:15:13 +0530

    feat: add toggle button and toggle filter list

---
+++ src/components/toggleFilterList/ToggleFilterList.tsx
@@ -0,0 +1,95 @@
+import { useCallback, useMemo, memo } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import uuid from 'react-native-uuid';
+
+import ToggleButton from '@components/toggleButton';
+import TextBlock from '@components/customText';
+import useStyles from '@hooks/useStyles';
+
+import toggleFilterListStyles from './styles';
+
+function renderFilters(
+  handleFilterToggle: any,
+  showIcon: boolean,
+  filters?: string[],
+  activeFilters?: string[],
+) {
+  return filters?.map(filter => (
+    <ToggleButton
+      key={uuid.v1().toString()}
+      title={filter}
+      toggleState={Boolean(
+        activeFilters?.find(oldFilter => oldFilter === filter),
+      )}
+      onToggle={toggleState => handleFilterToggle(filter, toggleState)}
+      showIcon={showIcon}
+    />
+  ));
+}
+
+function ToggleFilterList({
+  title,
+  filters,
+  activeFilters,
+  selectMultipleFilters,
+  onFiltersChange,
+  style,
+  textStyle,
+  scrollView,
+  scrollViewContent,
+}: ToggleFilterListProps) {
+  const styles = useStyles(toggleFilterListStyles);
+
+  const handleFilterToggle = useCallback(
+    (filter: string, toggleState: boolean) => {
+      let newActiveFilters: string[] = [];
+
+      const isFilterPresent = Boolean(
+        activeFilters.find(oldFilter => oldFilter === filter),
+      );
+
+      if (toggleState && !isFilterPresent) {
+        newActiveFilters = selectMultipleFilters
+          ? [...activeFilters, filter]
+          : [filter];
+      } else {
+        newActiveFilters = activeFilters.filter(
+          oldFilter => oldFilter !== filter,
+        );
+      }
+
+      onFiltersChange && onFiltersChange(newActiveFilters);
+    },
+    [onFiltersChange, activeFilters],
+  );
+
+  const filtersToRender = useMemo(() => {
+    return renderFilters(
+      handleFilterToggle,
+      Boolean(selectMultipleFilters),
+      filters,
+      activeFilters,
+    );
+  }, [filters, handleFilterToggle]);
+
+  return (
+    <View style={StyleSheet.compose(styles.scrollViewContainer, style)}>
+      <TextBlock style={StyleSheet.compose(styles.title, textStyle)}>
+        {title || 'Select item'}
+      </TextBlock>
+
+      <ScrollView
+        horizontal
+        contentContainerStyle={StyleSheet.compose(
+          styles.scrollViewContent,
+          scrollViewContent,
+        )}
+        style={scrollView}
+        showsHorizontalScrollIndicator={false}>
+        {filtersToRender}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default memo(ToggleFilterList);
---
+++ src/components/toggleFilterList/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+import { moderateScale, verticalScale } from '@utility/scalingHelpers';
+import { fontSize } from '@constants/fonts';
+
+function toggleFilterListStyles(colors: Colors) {
+  return StyleSheet.create({
+    scrollViewContainer: {
+      gap: moderateScale(15),
+    },
+
+    scrollViewContent: {
+      gap: moderateScale(10),
+    },
+
+    title: {
+      fontSize: fontSize.sixteen,
+    },
+  });
+}
+
+export default toggleFilterListStyles;
